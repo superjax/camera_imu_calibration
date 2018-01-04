2cff7fd12148cd752e50a006095e648a17d02f4f
James Jackson <superjax08@gmail.com>
2018-01-03 19:01:00 -0700

    rosified aruco stuff

---
+++ include/camera_imu_calibration.h
@@ -0,0 +1,45 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/aruco/charuco.hpp>
+#include <ros/ros.h>
+#include <vector>
+#include <iostream>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/Imu.h>
+#include <cv_bridge/cv_bridge.h>
+#include <image_transport/image_transport.h>
+
+using namespace cv;
+using namespace std;
+
+class Calibrator
+{
+public:
+    Calibrator();
+    ~Calibrator();
+    void image_callback(const sensor_msgs::ImageConstPtr& msg);
+    void imu_callback(const sensor_msgs::ImuConstPtr& msg);
+
+private:
+    void get_corners(const InputArray img, vector<Point2f>& charucoCorners, vector<int>& charucoIds, OutputArray tracked);
+
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_private_;
+    image_transport::ImageTransport it_;
+
+    image_transport::Publisher output_pub_;
+    image_transport::Subscriber image_sub_;
+    ros::Subscriber imu_sub_;
+
+    Mat camMatrix_;
+    Mat distCoeffs_;
+
+    // Create a bunch of aruco objects
+    Ptr<aruco::DetectorParameters> detectorParams_;
+    Ptr<aruco::Dictionary> dictionary_;
+
+    // create charuco board object
+    Ptr<aruco::CharucoBoard> charucoboard_;
+    Ptr<aruco::Board> board_;
+
+    bool show_tracked_ = false;
+};
--- src/camera_imu_calibration.cpp
+++ src/camera_imu_calibration.cpp
@@ -1,106 +1,141 @@
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/aruco/charuco.hpp>
-#include <vector>
-#include <iostream>
+#include "camera_imu_calibration.h"
 
 using namespace std;
 using namespace cv;
 
-int main(int argc, char *argv[])
+Calibrator::Calibrator() :
+    nh_private_("~"),
+    it_(nh_)
 {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/cv_camera/image_raw", 1, &Calibrator::image_callback, this);
+    output_pub_ = it_.advertise("/image_converter/tracked", 1);
 
+    nh_private_.param<bool>("show_tracked", show_tracked_, true);
 
-    VideoCapture input_video;
-    input_video.open(0);
-    if (!input_video.isOpened())
+    if (show_tracked_)
     {
-        cout << "no camera at /dev/video0\n";
-        return -1;
+        cv::namedWindow("tracked");
     }
-    else
-    {
-        cout << "found camera\n";
-    }
-
-    Mat camMatrix;
-    Mat distCoeffs;
 
     // ELP Web Cam
-//    camMatrix = (Mat1d(3, 3) <<  469.462257, 0.000000, 325.442775,
-//                                 0.000000, 470.773506, 230.956417,
-//                                 0.000000, 0.000000, 1.000000);
-//    distCoeffs = (Mat1d(1, 5) << -0.390234, 0.136303, -0.002855, 0.002160, 0.000000);
+    //    camMatrix_ = (Mat1d(3, 3) <<  469.462257, 0.000000, 325.442775,
+    //                                 0.000000, 470.773506, 230.956417,
+    //                                 0.000000, 0.000000, 1.000000);
+    //    distCoeffs_ = (Mat1d(1, 5) << -0.390234, 0.136303, -0.002855, 0.002160, 0.000000);
 
     // Create a bunch of aruco objects
-    Ptr<aruco::DetectorParameters> detectorParams = aruco::DetectorParameters::create();
-    Ptr<aruco::Dictionary> dictionary = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(aruco::DICT_6X6_50));
+    detectorParams_ = aruco::DetectorParameters::create();
+    dictionary_ = aruco::getPredefinedDictionary(aruco::PREDEFINED_DICTIONARY_NAME(aruco::DICT_6X6_50));
 
     // create charuco board object
-    Ptr<aruco::CharucoBoard> charucoboard = aruco::CharucoBoard::create(5, 7, 0.0351f, 0.0222f, dictionary);
-    Ptr<aruco::Board> board = charucoboard.staticCast<aruco::Board>();
+    charucoboard_ = aruco::CharucoBoard::create(5, 7, 0.0351f, 0.0222f, dictionary_);
+    board_ = charucoboard_.staticCast<aruco::Board>();
+}
 
+Calibrator::~Calibrator()
+{
+    if (show_tracked_)
+    {
+        cv::destroyWindow("tracked");
+    }
+}
+
+void Calibrator::image_callback(const sensor_msgs::ImageConstPtr &msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_FATAL("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    // Track Corners
+    vector<Point2f> charucoCorners;
+    vector<int> charucoIds;
+    Mat tracked;
+    get_corners(cv_ptr->image, charucoCorners, charucoIds, tracked);
+
+    // Output modified video stream
+    cv_ptr->image = tracked;
+    output_pub_.publish(cv_ptr->toImageMsg());
+}
+
+void Calibrator::get_corners(const InputArray image, vector<Point2f>& charucoCorners, vector<int>& charucoIds,
+                             OutputArray tracked=noArray())
+{
     // length of axis when drawing pose
     float axisLength = 0.5f * (5 * 0.0351);
 
     bool draw_rejected = false;
 
+    // Containers to hold marker stuff
+    vector<int> markerIds;
+    vector<vector<Point2f>> markerCorners, rejectedMarkers;
 
-    while(input_video.grab())
+    // detect markers
+    aruco::detectMarkers(image, dictionary_, markerCorners, markerIds, detectorParams_,
+                         rejectedMarkers);
+
+    // refind strategy to detect more markers
+    aruco::refineDetectedMarkers(image, board_, markerCorners, markerIds, rejectedMarkers,
+                                 camMatrix_, distCoeffs_);
+
+    // interpolate charuco corners
+    int interpolatedCorners = 0;
+    if(markerIds.size() > 0)
+        interpolatedCorners =
+                aruco::interpolateCornersCharuco(markerCorners, markerIds, image, charucoboard_,
+                                                 charucoCorners, charucoIds, camMatrix_, distCoeffs_);
+
+    // draw results if an output array was supplied
+    Mat imageCopy;
+    image.copyTo(imageCopy);
+    if(markerIds.size() > 0)
     {
-        Mat image, imageCopy;
-        input_video.retrieve(image);
-
-        vector<int> markerIds, charucoIds;
-        vector<vector<Point2f>> markerCorners, rejectedMarkers;
-        vector<Point2f> charucoCorners;
-        Vec3d rvec, tvec;
-
-        // detect markers
-        aruco::detectMarkers(image, dictionary, markerCorners, markerIds, detectorParams,
-                             rejectedMarkers);
-
-        // refind strategy to detect more markers
-        aruco::refineDetectedMarkers(image, board, markerCorners, markerIds, rejectedMarkers,
-                                         camMatrix, distCoeffs);
-
-        // interpolate charuco corners
-        int interpolatedCorners = 0;
-        if(markerIds.size() > 0)
-            interpolatedCorners =
-                aruco::interpolateCornersCharuco(markerCorners, markerIds, image, charucoboard,
-                                                 charucoCorners, charucoIds, camMatrix, distCoeffs);
-
-        // estimate charuco board pose (requires camera intrinsics)
-        bool validPose = false;
-        if(camMatrix.total() != 0)
-            validPose = aruco::estimatePoseCharucoBoard(charucoCorners, charucoIds, charucoboard,
-                                                        camMatrix, distCoeffs, rvec, tvec);
-
-        // draw results
-        image.copyTo(imageCopy);
-        if(markerIds.size() > 0)
-        {
-            aruco::drawDetectedMarkers(imageCopy, markerCorners);
-        }
-
-        // Draw the rejected markers
-        if (draw_rejected)
-            aruco::drawDetectedMarkers(imageCopy, rejectedMarkers, noArray(), Scalar(100, 0, 255));
-
-        if(interpolatedCorners > 0)
-        {
-            Scalar color;
-            color = Scalar(255, 0, 150);
-            aruco::drawDetectedCornersCharuco(imageCopy, charucoCorners, charucoIds, color);
-        }
-
-        if(validPose)
-            aruco::drawAxis(imageCopy, camMatrix, distCoeffs, rvec, tvec, axisLength);
-
-        imshow("out", imageCopy);
-        char key = (char)waitKey(1);
-        if(key == 'q') break;
+        aruco::drawDetectedMarkers(imageCopy, markerCorners);
     }
 
+    // Draw the rejected markers
+    if (draw_rejected)
+        aruco::drawDetectedMarkers(imageCopy, rejectedMarkers, noArray(), Scalar(100, 0, 255));
+
+    if(interpolatedCorners > 0)
+    {
+        Scalar color;
+        color = Scalar(255, 0, 150);
+        aruco::drawDetectedCornersCharuco(imageCopy, charucoCorners, charucoIds, color);
+    }
+
+    // estimate and draw charuco board pose (requires camera intrinsics)
+    if(camMatrix_.total() != 0)
+    {
+        Vec3d rvec, tvec;
+        if ( aruco::estimatePoseCharucoBoard(charucoCorners, charucoIds, charucoboard_,
+                                             camMatrix_, distCoeffs_, rvec, tvec) )
+        {
+            aruco::drawAxis(imageCopy, camMatrix_, distCoeffs_, rvec, tvec, axisLength);
+        }
+    }
+    imageCopy.copyTo(tracked);
+
+    // Update GUI Window
+    if (show_tracked_)
+    {
+        cv::imshow("tracked", tracked);
+        cv::waitKey(3);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    ros::init(argc, argv, "camera_imu_calibrator");
+    Calibrator thing;
+    ros::spin();
     return 0;
 }
+
