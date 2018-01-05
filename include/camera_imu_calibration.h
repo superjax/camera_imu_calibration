48761c6faebe98537da50d49a255856d40e0d5c7
James Jackson <superjax08@gmail.com>
2018-01-05 13:35:50 -0700

    working on GTSAM stuff

--- include/camera_imu_calibration.h
+++ include/camera_imu_calibration.h
@@ -1,16 +1,39 @@
+#include <vector>
+#include <deque>
+#include <iostream>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/aruco/charuco.hpp>
+
+#include <Eigen/Core>
+
 #include <ros/ros.h>
 #include <ros/package.h>
-#include <vector>
-#include <iostream>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/Imu.h>
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/navigation/GPSFactor.h>
+#include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/slam/dataset.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/inference/Symbol.h>
+
 using namespace cv;
 using namespace std;
+using namespace Eigen;
+namespace gt = gtsam;
+
+
+using gt::symbol_shorthand::X; // Pose3 (x,y,z,r,p,y)
+using gt::symbol_shorthand::V; // Vel   (xdot,ydot,zdot)
+using gt::symbol_shorthand::B; // Bias  (ax,ay,az,gx,gy,gz)
+using gt::symbol_shorthand::L; // Landmarks (x, y, z)
 
 class Calibrator
 {
@@ -21,7 +44,29 @@ public:
     void imu_callback(const sensor_msgs::ImuConstPtr& msg);
 
 private:
+
+    typedef struct
+    {
+        Vector3d acc;
+        Vector3d gyro;
+        uint64_t usec;
+    } imu_measurement_t;
+
+    typedef struct
+    {
+        vector<Point2f> corner_positions;
+        vector<int> ids;
+        uint64_t usec;
+    } camera_measurement_t;
+
+    deque<imu_measurement_t> imu_queue_;
+    camera_measurement_t camera_meas_;
+    bool new_camera_;
+
     void get_corners(const InputArray img, vector<Point2f>& charucoCorners, vector<int>& charucoIds, OutputArray tracked);
+    void process_measurement_queues();
+    void initialize_graph(const gt::Pose3 initial_pose);
+    void add_measurement_to_graph(const Vector3d &dtheta, const Vector3d &dvel, const vector<Point2f> &corners, const vector<int> &ids);
 
     ros::NodeHandle nh_;
     ros::NodeHandle nh_private_;
@@ -43,4 +88,11 @@ private:
     Ptr<aruco::Board> board_;
 
     bool show_tracked_ = false;
+
+    // GTSAM Factor Graph objects
+    gt::Values initial_values_;
+    int num_poses_ = 0;
+
+    gt::NonlinearFactorGraph graph_;
+    gt::PreintegratedCombinedMeasurements *imu_preintegrated_;
 };
--- src/camera_imu_calibration.cpp
+++ src/camera_imu_calibration.cpp
@@ -7,6 +7,7 @@ Calibrator::Calibrator() :
     nh_private_("~"),
     it_(nh_)
 {
+    imu_sub_ = nh_.subscribe("imu", 100, &Calibrator::imu_callback, this);
     image_sub_ = it_.subscribe("/cv_camera/image_raw", 1, &Calibrator::image_callback, this);
     output_pub_ = it_.advertise("/image_converter/tracked", 1);
 
@@ -16,7 +17,7 @@ Calibrator::Calibrator() :
         cv::namedWindow("tracked");
     }
 
-    // Load intrinsics
+    // Load camera intrinsics
     string intrinsics_path;
     nh_private_.param<string>("intrinsics_path", intrinsics_path, ros::package::getPath("camera_imu_calibration")+"/param/elp.yaml");
     FileStorage fs(intrinsics_path, FileStorage::READ);
@@ -63,9 +64,24 @@ void Calibrator::image_callback(const sensor_msgs::ImageConstPtr &msg)
     Mat tracked;
     get_corners(cv_ptr->image, charucoCorners, charucoIds, tracked);
 
+    // Add Measurement to Queue
+    camera_meas_.corner_positions = charucoCorners;
+    camera_meas_.ids = charucoIds;
+    camera_meas_.usec = msg->header.stamp.toNSec();
+    new_camera_ = true;
+
     // Output modified video stream
     cv_ptr->image = tracked;
     output_pub_.publish(cv_ptr->toImageMsg());
+}
+
+void Calibrator::imu_callback(const sensor_msgs::ImuConstPtr &msg)
+{
+    imu_measurement_t meas;
+    meas.acc << msg->linear_acceleration.x, msg->linear_acceleration.y, msg->linear_acceleration.z;
+    meas.gyro << msg->angular_velocity.x, msg->angular_velocity.y, msg->angular_velocity.z;
+    meas.usec = msg->header.stamp.toNSec();
+    imu_queue_.push_back(meas);
 }
 
 void Calibrator::get_corners(const InputArray image, vector<Point2f>& charucoCorners, vector<int>& charucoIds,
@@ -132,6 +148,113 @@ void Calibrator::get_corners(const InputArray image, vector<Point2f>& charucoCor
         cv::imshow("tracked", tracked);
         cv::waitKey(3);
     }
+}
+
+void Calibrator::process_measurement_queues()
+{
+    // If we have a new camera measurement, integrate all the IMU data between these frames,
+    // but check to make sure that we have all the data between these frames
+    // by making sure that we have at least one IMU measurement which comes after the image
+    // TODO: propagate right up to the frame, and save for later, rather than integrating to the closest previous IMU measurement
+    if (new_camera_)
+    {
+        if (imu_queue_.end()->usec > camera_meas_.usec)
+        {
+            Vector3d dtheta;
+            Vector3d dvel;
+            while (imu_queue_.begin()->usec < camera_meas_.usec)
+            {
+                imu_measurement_t meas = imu_queue_.front();
+                imu_queue_.pop_front();
+                uint64_t dt = (imu_queue_.begin())->usec - meas.usec;
+                dtheta += dt * meas.gyro;
+                dvel += dt * meas.acc;
+            }
+
+            // We now have a new measurement to add to the graph
+            add_measurement_to_graph(dtheta, dvel, camera_meas_.corner_positions, camera_meas_.ids);
+            new_camera_ = false;
+        }
+    }
+}
+
+void Calibrator::initialize_graph(const gtsam::Pose3 initial_pose)
+{
+    gt::Vector3 prior_velocity(0.0, 0.0, 0.0);
+    gt::imuBias::ConstantBias prior_imu_bias; // assume zero
+
+    // Add Origin to initial guess
+    initial_values_.insert(X(num_poses_), initial_pose);
+    initial_values_.insert(V(num_poses_), prior_velocity);
+    initial_values_.insert(B(num_poses_), prior_imu_bias);
+
+    // Create noise estimates for initial pose, velocity and bias
+    gt::noiseModel::Diagonal::shared_ptr pose_noise_model = gt::noiseModel::Diagonal::Sigmas((gt::Vector(6) << 0.01, 0.01, 0.01, 0.5, 0.5, 0.5).finished()); // rad,rad,rad, m, m, m
+    gt::noiseModel::Diagonal::shared_ptr velocity_noise_model = gt::noiseModel::Isotropic::Sigma(3,0.1); // m/s
+    gt::noiseModel::Diagonal::shared_ptr bias_noise_model = gt::noiseModel::Isotropic::Sigma(6,1e-3);
+
+    // Add all prior factors to the graph
+    graph_.add(gt::PriorFactor<gt::Pose3>(X(num_poses_), initial_pose, pose_noise_model));
+    graph_.add(gt::PriorFactor<gt::Vector3>(V(num_poses_), prior_velocity,velocity_noise_model));
+    graph_.add(gt::PriorFactor<gt::imuBias::ConstantBias>(B(num_poses_), prior_imu_bias,bias_noise_model));
+
+    // Add chessboard corner XYZ positions to initial_values and graph
+    gt::noiseModel::Diagonal::shared_ptr corner_noise_model = gt::noiseModel::Isotropic::Sigma(3,1e-4); // m
+    for (int row = 0; row < 5; row++)
+    {
+        for (int col = 0; col < 7; col++)
+        {
+            gt::Point3 marker_point(0.0351f * (row+1), 0.0351f * (col+1), 0.0);
+            int corner_num = row + (5*col);
+            initial_values_.insert(L(corner_num), marker_point);
+            graph_.add(gt::PriorFactor<gt::Point3>(L(corner_num), marker_point ,corner_noise_model));
+        }
+    }
+
+    // We use the sensor specs to build the noise model for the IMU factor.
+    double accel_noise_sigma = 0.0003924;
+    double gyro_noise_sigma = 0.000205689024915;
+    double accel_bias_rw_sigma = 0.004905;
+    double gyro_bias_rw_sigma = 0.000001454441043;
+    gt::Matrix33 measured_acc_cov = gt::Matrix33::Identity(3,3) * pow(accel_noise_sigma,2);
+    gt::Matrix33 measured_omega_cov = gt::Matrix33::Identity(3,3) * pow(gyro_noise_sigma,2);
+    gt::Matrix33 integration_error_cov = gt::Matrix33::Identity(3,3)*1e-8; // error committed in integrating position from velocities
+    gt::Matrix33 bias_acc_cov = gt::Matrix33::Identity(3,3) * pow(accel_bias_rw_sigma,2);
+    gt::Matrix33 bias_omega_cov = gt::Matrix33::Identity(3,3) * pow(gyro_bias_rw_sigma,2);
+    gt::Matrix66 bias_acc_omega_int = gt::Matrix::Identity(6,6)*1e-5; // error in the bias used for preintegration
+
+    // Create a NED preintegration shared parameter
+    boost::shared_ptr<gt::PreintegratedCombinedMeasurements::Params> p = gt::PreintegratedCombinedMeasurements::Params::MakeSharedD(0.0);
+    // PreintegrationBase params:
+    p->accelerometerCovariance = measured_acc_cov; // acc white noise in continuous
+    p->integrationCovariance = integration_error_cov; // integration uncertainty continuous
+    // should be using 2nd order integration
+    // PreintegratedRotation params:
+    p->gyroscopeCovariance = measured_omega_cov; // gyro white noise in continuous
+    // PreintegrationCombinedMeasurements params:
+    p->biasAccCovariance = bias_acc_cov; // acc bias in continuous
+    p->biasOmegaCovariance = bias_omega_cov; // gyro bias in continuous
+    p->biasAccOmegaInt = bias_acc_omega_int;
+
+
+    imu_preintegrated_ = new gt::PreintegratedCombinedMeasurements(p, prior_imu_bias);
+
+
+    // Store previous state for the imu integration and the latest predicted outcome.
+    NavState prev_state(prior_pose, prior_velocity);
+    NavState prop_state = prev_state;
+    imuBias::ConstantBias prev_bias = prior_imu_bias;
+
+
+
+
+
+}
+
+
+void Calibrator::add_measurement_to_graph(const Vector3d& dtheta, const Vector3d& dvel, const vector<Point2f>& corners, const vector<int>& ids)
+{
+
 }
 
 int main(int argc, char* argv[])
